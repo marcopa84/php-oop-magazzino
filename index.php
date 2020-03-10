24a951bdf755e03e0f8d619486db7ea202612246
Marco <marcopa84@hotmail.it>
2020-03-10 17:33:35 +0100

    type of ithem done!

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
 require_once 'ithems.php';
 
+$nikeSqualo = new Shoes();
+
+
+$nikeSqualo->type = 'sneakers';
+$nikeSqualo->bar_code = '080123456789';
+$nikeSqualo->brand = 'Nike';
+$nikeSqualo->size = 46;
+$nikeSqualo->color = 'Black';
+$nikeSqualo->price = '138,90';
+
+
+
+var_dump($nikeSqualo);
